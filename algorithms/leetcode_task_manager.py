453bc90d57e854ffa515ab6b898fa8f085919a93
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2025-09-18 23:01:46 +0200

    Added a simple and efficient task manager

--- algorithms/leetcode_task_manager.py
+++ algorithms/leetcode_task_manager.py
@@ -3,36 +3,36 @@ import heapq
 
 class TaskManager:
     def __init__(self, tasks):
-        self.taskId_to_priority = {}
-        self.taskId_to_userId = {}
+        self.task_id_to_priority = {}
+        self.task_id_to_user_id = {}
         self.priority_heap = []
 
         for task in tasks:
-            userId, taskId, priority = task
-            self.taskId_to_priority[taskId] = priority
-            self.taskId_to_userId[taskId] = userId
-            heapq.heappush(self.priority_heap, (-priority, -taskId, taskId))
+            user_id, task_id, priority = task
+            self.task_id_to_priority[task_id] = priority
+            self.task_id_to_user_id[task_id] = user_id
+            heapq.heappush(self.priority_heap, (-priority, -task_id, task_id))
 
-    def add(self, userId, taskId, priority):
-        self.taskId_to_priority[taskId] = priority
-        self.taskId_to_userId[taskId] = userId
-        heapq.heappush(self.priority_heap, (-priority, -taskId, taskId))
+    def add(self, user_id, task_id, priority):
+        self.task_id_to_priority[task_id] = priority
+        self.task_id_to_user_id[task_id] = user_id
+        heapq.heappush(self.priority_heap, (-priority, -task_id, task_id))
 
-    def edit(self, taskId, newPriority):
-        self.taskId_to_priority[taskId] = newPriority
-        heapq.heappush(self.priority_heap, (-newPriority, -taskId, taskId))
+    def edit(self, task_id, new_priority):
+        self.task_id_to_priority[task_id] = new_priority
+        heapq.heappush(self.priority_heap, (-new_priority, -task_id, task_id))
 
-    def rmv(self, taskId) -> None:
-        if taskId in self.taskId_to_priority:
-            del self.taskId_to_priority[taskId]
-            del self.taskId_to_userId[taskId]
+    def rmv(self, task_id) -> None:
+        if task_id in self.task_id_to_priority:
+            del self.task_id_to_priority[task_id]
+            del self.task_id_to_user_id[task_id]
 
-    def execTop(self):
+    def exec_top(self):
         while self.priority_heap:
-            negPriority, negTaskId, taskId = heapq.heappop(self.priority_heap)
-            if taskId in self.taskId_to_priority and self.taskId_to_priority[taskId] == -negPriority:
-                userId = self.taskId_to_userId[taskId]
-                del self.taskId_to_priority[taskId]
-                del self.taskId_to_userId[taskId]
-                return userId
+            neg_priority, neg_task_id, task_id = heapq.heappop(self.priority_heap)
+            if task_id in self.task_id_to_priority and self.task_id_to_priority[task_id] == -neg_priority:
+                user_id = self.task_id_to_user_id[task_id]
+                del self.task_id_to_priority[task_id]
+                del self.task_id_to_user_id[task_id]
+                return user_id
         return -1
