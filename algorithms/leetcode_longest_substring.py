9bc01dd1447783137b14968d193f0550b71d65f4
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2025-09-25 20:23:45 +0200

    Added a function that returns the longest substring without repeating characters

---
+++ algorithms/leetcode_longest_substring.py
@@ -0,0 +1,18 @@
+def length_of_longest_substring(s: str) -> int:
+    left = 0
+    seen = set()
+    max_length = 0
+
+    for right in range(len(s)):
+        while s[right] in seen:
+            seen.remove(s[left])
+            left += 1
+        seen.add(s[right])
+        max_length = max(max_length, right - left + 1)
+
+    return max_length
+
+
+print(length_of_longest_substring("abcabcbb"))
+print(length_of_longest_substring("bbbbb"))
+print(length_of_longest_substring("pwwkew"))
