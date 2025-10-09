75daa6f812b3a5747cbbc3238890f9a8fa774a4a
RangelGasharov <gasharov@outlook.com>
2025-10-09 22:51:05 +0200

    Added the template for the trie class

---
+++ algorithms/leetcode_implement_trie.py
@@ -0,0 +1,13 @@
+class Trie:
+
+    def __init__(self):
+        print()
+
+    def insert(self, word: str) -> None:
+        print()
+
+    def search(self, word: str) -> bool:
+        return True
+
+    def startsWith(self, prefix: str) -> bool:
+        return True
