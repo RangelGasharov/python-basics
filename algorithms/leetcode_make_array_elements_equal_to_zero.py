882f5d052ca8694340948ea73a5ba5a47577e3f2
RangelGasharov <gasharov@outlook.com>
2025-10-28 23:39:37 +0100

    Added an improved version of the algorithm

--- algorithms/leetcode_make_array_elements_equal_to_zero.py
+++ algorithms/leetcode_make_array_elements_equal_to_zero.py
@@ -1,32 +1,27 @@
-from itertools import groupby
-
-
-def countValidSelections(nums: list[int]) -> int:
+def count_valid_selections(nums: list[int]) -> int:
     valid_selections = 0
     indices = []
+    n = len(nums)
+    sum_left = {0: nums[0]}
+    sum_right = {n - 1: nums[n - 1]}
+
+    for i in range(1, len(nums)):
+        sum_left[i] = sum_left[i - 1] + nums[i]
+
+    for i in range(n - 2, -1, -1):
+        sum_right[i] = sum_right[(i + 1)] + nums[i]
 
     for i in range(len(nums)):
         if nums[i] == 0:
             indices.append(i)
 
-    def is_valid(numbers: list, position: int, initial_direction: int):
-        nums = list(numbers)
-        current_direction = initial_direction
-        i = position + current_direction
-        while 0 <= i < len(numbers):
-            if nums[i] > 0:
-                current_direction = -current_direction
-                nums[i] -= 1
-                i += current_direction
-            else:
-                i += current_direction
-        g = groupby(nums)
-        return next(g, True) and not next(g, False)
-
     for index in indices:
-        valid_selections += 1 * (is_valid(nums, index, -1))
-        valid_selections += 1 * (is_valid(nums, index, 1))
+        left_sum = sum_left.get(index - 1, 0)
+        right_sum = sum_right.get(index + 1, 0)
+        valid_selections += left_sum == right_sum or left_sum == right_sum + 1
+        valid_selections += left_sum == right_sum or left_sum == right_sum - 1
     return valid_selections
 
 
-print(countValidSelections([1, 0, 2, 0, 3]))
+print(count_valid_selections([1, 0, 2, 0, 3]))
+print(count_valid_selections([2,3,4,0,4,1,0]))
