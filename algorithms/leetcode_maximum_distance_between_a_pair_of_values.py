a5956db8594d36b3835cad0753bb461c9dbce8a1
RangelGasharov <gasharov@outlook.com>
2026-04-19 22:57:17 +0200

    Refactoring - removed unnecessary import and renamed funtion name

--- algorithms/leetcode_maximum_distance_between_a_pair_of_values.py
+++ algorithms/leetcode_maximum_distance_between_a_pair_of_values.py
@@ -1,18 +1,17 @@
-from bisect import bisect_left
 from typing import List
 
 
-def maxDistance(nums1: List[int], nums2: List[int]) -> int:
-    i = j = max_distance = 0
+def max_distance(nums1: List[int], nums2: List[int]) -> int:
+    i = j = max_dist = 0
     while i < len(nums1) and j < len(nums2):
         if nums1[i] <= nums2[j]:
-            max_distance = max(max_distance, j - i)
+            max_dist = max(max_dist, j - i)
             j += 1
         else:
             i += 1
-    return max_distance
+    return max_dist
 
 
-print(maxDistance([55, 30, 5, 4, 2], [100, 20, 10, 10, 5]))
-print(maxDistance([2, 2, 2], [10, 10, 1]))
-print(maxDistance([30, 29, 19, 5], [25, 25, 25, 25, 25]))
+print(max_distance([55, 30, 5, 4, 2], [100, 20, 10, 10, 5]))
+print(max_distance([2, 2, 2], [10, 10, 1]))
+print(max_distance([30, 29, 19, 5], [25, 25, 25, 25, 25]))
