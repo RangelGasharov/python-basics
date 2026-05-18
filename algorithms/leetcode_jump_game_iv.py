8e77158bfc82ff20138db725f479db51f169e075
RangelGasharov <gasharov@outlook.com>
2026-05-18 22:40:50 +0200

    Slved the jump game iv problem

---
+++ algorithms/leetcode_jump_game_iv.py
@@ -0,0 +1,44 @@
+from collections import defaultdict, deque
+from typing import List
+
+
+def min_jumps(arr: List[int]) -> int:
+    n = len(arr)
+    if n == 1:
+        return 0
+
+    mp = defaultdict(list)
+    for i in range(n):
+        mp[arr[i]].append(i)
+
+    q = deque([(0, 0)])
+    vis = [0] * n
+    vis[0] = 1
+
+    while q:
+        node, dist = q.popleft()
+
+        if node == n - 1:
+            return dist
+
+        if node - 1 >= 0 and not vis[node - 1]:
+            vis[node - 1] = 1
+            q.append((node - 1, dist + 1))
+
+        if node + 1 < n and not vis[node + 1]:
+            vis[node + 1] = 1
+            q.append((node + 1, dist + 1))
+
+        for nxt in mp[arr[node]]:
+            if not vis[nxt]:
+                vis[nxt] = 1
+                q.append((nxt, dist + 1))
+
+        mp[arr[node]].clear()
+
+    return -1
+
+
+print(min_jumps([100, -23, -23, 404, 100, 23, 23, 23, 3, 404]))
+print(min_jumps([7]))
+print(min_jumps([7, 6, 9, 6, 9, 6, 9, 7]))
